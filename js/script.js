3af7c299195947a50d06904c33ce4e9f37429e53
skhodjaev <skhodjaev@icloud.com>
2024-05-26 21:05:36 +0500

    added stats section and increment js

--- js/script.js
+++ js/script.js
@@ -10,4 +10,28 @@ function userScroll() {
   });
 }
 
+function incrementStats() {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+
 document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
