fe5e7f6c34b5661ec039ae22cd74f91cb9066ed2
skhodjaev <skhodjaev@icloud.com>
2024-05-27 17:15:50 +0500

    added scroll to top btn with js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 function userScroll() {
   const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('.to-top-btn');
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       navbar.classList.add('navbar-sticky');
+      toTopBtn.classList.add('show');
     } else {
       navbar.classList.remove('navbar-sticky');
+      toTopBtn.classList.remove('show');
     }
   });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function incrementStats() {
@@ -33,5 +41,7 @@ function incrementStats() {
   });
 }
 
+// Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
